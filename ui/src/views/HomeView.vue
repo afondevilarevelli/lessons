5e7a76099109897c728d4ed9019f0dcac9e1d85b
afondevilarevelli <antonny.42@gmail.com>
2023-12-29 16:18:01 -0300

    Added tailwind css and daisy ui

--- ui/src/views/HomeView.vue
+++ ui/src/views/HomeView.vue
@@ -1,9 +1,15 @@
-<script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
-
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <div class="about">
+    <button class="btn">Button</button>
+  </div>
 </template>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
